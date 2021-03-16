c438993be7c41846190ce36481b054590c8d738f
Vovanus0372 <318221@college.local>
2021-03-16 09:20:07 +0300

    add class count

---
+++ Count.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabotai
+{//
+    class Count
+    {
+        public string name;
+        public string shifr;
+        public int sum;
+        public Count()
+        {
+            this.name = "Изделие";
+            this.shifr = "Poof";
+            this.sum = 0;
+        }
+        public Count(string cName, string cShifr, int cSum)
+        {
+            this.name = cName;
+            this.shifr = cShifr;
+            this.sum = cSum;
+        }
+    }
+}
